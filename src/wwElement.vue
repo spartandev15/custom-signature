573da6cde57fbc1e119b34f9a87faac44692e3be
spartandev15 <botsspartan@gmail.com>
2023-09-22 13:37:26 +0530

    skdjf

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ww-color-picker">
-    <input type="text"  v-model="text"/>
-   <wwElement v-bind="content.labelComponent" :ww-props="{text:color || ''}"/>
+    <input type="text" v-model="text" @input="convertToImage" required />
+    <img v-if="image" :src="image" alt="Image" />
   </div>
 </template>
 
@@ -9,33 +9,50 @@
 export default {
   props: {
     content: { type: Object, required: true },
-    uid:{type:Object, required:true},
+    uid: { type: Object, required: true },
   },
-  setup(props) {
-    const { value, setValue } = wwLib.wwVariable.useComponentVariable({
-            uid: props.uid,
-            name: 'value',
-            type: 'string',
-            defaultValue: null
-        });
-        return {value,setValue}
+  data() {
+    return {
+      value: '', // Store the value as a data property
+      text: '', // Input text
+      image: null, // Image data URL
+    };
   },
-  computed: {
-    text:{ 
-      get(){
-        return typeof this.value === 'string'? this.value:''
-      },
-      set(value){
-this.setValue(value)
-      }
-    }
+  methods: {
+    convertToImage() {
+      // Create a new canvas element
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+
+      // Define the canvas dimensions and style
+      canvas.width = 200; // Set the desired width
+      canvas.height = 60; // Set the desired height
+      context.font = 'italic 30px Cookie';
+      context.fillStyle = 'black';
+      context.fontWeight = '500';
+
+      // Clear the canvas
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      // Draw the text on the canvas
+      context.fillText(this.text, 10, 40);
+
+      // Convert the canvas to a data URL
+      const dataURL = canvas.toDataURL('image/png');
+
+      // Set the data URL as the image source
+      this.image = dataURL;
+
+      // Update the value (if needed)
+      this.value = this.text;
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .ww-color-picker {
-display: flex;
-align-items: center;
+  display: flex;
+  align-items: center;
 }
 </style>
